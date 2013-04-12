33e1277e73066366927229b86949082dd4bdafc7
Jacob Bang <julemand101@gmail.com>
2013-04-12 14:49:03 +0200

    Added documentation for the logging variable

--- lib/distributed_dart.dart
+++ lib/distributed_dart.dart
@@ -12,6 +12,12 @@ part 'src/dart_import/util/characters.dart';
 part 'src/network/network.dart';
 
 // logging mechanism instad of print
+
+/**
+ * Set to true for enabling debug output from the distributed_dart library.
+ * Default value is false.
+ */
 bool logging = false;
+
 _log(String msg) =>  logging ? stdout.write("DIST_DART, log: ${msg}\n") : "";
 _err(String msg) =>  logging ? stderr.write("DIST_DART, err: ${msg}\n") : "";
