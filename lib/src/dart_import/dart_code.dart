8a909f3cc67c60bcf225789cb43ec8c77411ca74
Jacob Bang <julemand101@gmail.com>
2013-05-01 12:41:40 +0200

    Added a lot to dart_code.dart
    Makes the class more useful. :)

--- lib/src/dart_import/dart_code.dart
+++ lib/src/dart_import/dart_code.dart
@@ -1,15 +1,83 @@
 part of distributed_dart;
 
 class DartCode {
-  String name;
-  String path;
-  String hash;
-  List<int> source;
-  List<DartCode> dependencies = new List<DartCode>();
+  static const String NAME = "name";
+  static const String PATH = "path";
+  static const String HASH = "hash";
+  static const String DEPENDENCIES = "dependencies";
   
-  String get sourceAsString {
-    StringBuffer sb = new StringBuffer();
-    source.forEach((e) => sb.writeCharCode(e));
-    return sb.toString();
+  final String name;
+  final String path;
+  final List<int> _hash;
+  final List<DartCode> _dependencies;
+  
+  /**
+   * Create DartCode instance. Should only be used by [DartCodeDb].
+   */
+  const DartCode(String this.name, String this.path, 
+      List<int> this._hash, List<DartCode> this._dependencies);
+  
+  /**
+   * Create DartCode object from JSON String.
+   */
+  factory DartCode.fromJson(String jsonString) {
+    return new DartCode.fromJsonMap(json.parse(jsonString));
+  }
+  
+  /**
+   * Create DartCode object from Map object (from json.parse())
+   */
+  factory DartCode.fromJsonMap(Map jMap) {
+    List<DartCode> dependenciesList;
+    
+    if (jMap.containsKey(DEPENDENCIES) && jMap[DEPENDENCIES] != null) {
+      dependenciesList = new List<DartCode>();
+      
+      jMap[DEPENDENCIES].forEach((var jDartCodeMap) {
+        if (jDartCodeMap != null) {
+          dependenciesList.add(new DartCode.fromJsonMap(jDartCodeMap));
+        }
+      });
+    }
+    
+    return new DartCode(jMap[NAME], jMap[PATH], jMap[HASH], dependenciesList);
+  }
+  
+  List<DartCode> get dependencies {
+    return _dependencies.toList(growable: false);
+  }
+  
+  List<int> get fileHash {
+    return _hash.toList(growable: false);
+  }
+  
+  String get fileHashAsString {
+    return _hashListToString(_hash);
+  }
+  
+  List<int> get dartCodeHash {
+    SHA1 sum = new SHA1();
+    sum.add(name.codeUnits);
+    sum.add(_hash);
+    _dependencies.forEach((DartCode x) => sum.add(x._hash));
+    return sum.close();
+  }
+  
+  String get dartCodeHashAsString {
+    return _hashListToString(dartCodeHash);
+  }
+  
+  /**
+   * Generate Map object from DartCode instance.
+   */
+  Map<String, Object> toJson() {
+    var returnMap = new Map();
+    
+    returnMap[NAME] = this.name;
+    returnMap[PATH] = this.path;
+    returnMap[HASH] = this._hash;
+    returnMap[DEPENDENCIES] = this._dependencies;  
+    
+    return returnMap;
   }
 }
