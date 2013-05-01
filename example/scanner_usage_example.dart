46f7d22cfa94bd7fd102b41db71dfbaf92d2b065
Jacob Bang <julemand101@gmail.com>
2013-05-01 12:48:04 +0200

    Changed the scanner example
    Not pretty but it works...

--- example/scanner_usage_example.dart
+++ example/scanner_usage_example.dart
@@ -7,6 +7,7 @@
 import "package:distributed_dart/distributed_dart.dart" as dist;
 import "dart:io";
 import "dart:crypto";
+import "dart:json" as json;
 
 void main() {
   File f = new File.fromPath(new Path("lib/distributed_dart.dart"));
@@ -18,7 +19,8 @@ void main() {
     SHA1 checksum = new SHA1();
     
     checksum.add(filecontent);
-    print("Checksum: ${checksum.close()}");
+    List<int> checksumint = checksum.close();
+    print("Checksum: $checksumint}");
     
     Runes r = (new String.fromCharCodes(filecontent)).runes;
     
@@ -27,8 +29,29 @@ void main() {
 //    });
     
     dist.Scanner s = new dist.Scanner(r);
-    s.scan();
-    s.paths.forEach((e) => print(e));
+    s.getDependencies().forEach((e) => print(e));
     //print(s.byteOffset);
+    
+    // DartCode(this.name, this.path, this.hash, this.dependencies);
+    dist.DartCode a = new dist.DartCode("Name a", "/usr/bin", [ 1, 2, 3], null);
+    dist.DartCode b = new dist.DartCode("Name b", "/usr/bin", [ 1, 2, 3], [a]);
+    dist.DartCode c = new dist.DartCode("Name c", "/usr/bin", [ 1, 2, 3], [b]);
+    dist.DartCode d = new dist.DartCode("Name d", "/usr/bin", [ 1, 2, 3], null);
+    dist.DartCode e = new dist.DartCode("Name e", "/usr/bin", [ 1, 2, 3], [d, c]);
+    
+    print(json.stringify(e));
+    
+    dist.DartCode k = new dist.DartCode.fromJson(json.stringify(e));
+    
+    k.dependencies.forEach((dist.DartCode x) => print(x.name));
+    print(k.fileHash);
+    print(k.dartCodeHash);
+    print(k.dartCodeHashAsString);
+    print(f.fullPathSync());
+    
+    dist.DartCodeDb db = new dist.DartCodeDb();
+    db.getSource(new dist.DartCode("scanner_usage_example.dart", "lib/distributed_dart.dart", checksumint, [])).then((List<int> t) => t.forEach((int x) => print(new String.fromCharCode(x))));
+    
+    db.resolve("lib/distributed_dart.dart", useCache:true);
   });
 }
