a26136f993747f877010936a71297318ee877a3a
Jacob Bang <julemand101@gmail.com>
2013-05-13 07:13:26 +0200

    Need dart:collection for HashList

--- lib/distributed_dart.dart
+++ lib/distributed_dart.dart
@@ -5,6 +5,7 @@ import 'dart:isolate';
 import 'dart:json' as json;
 import 'dart:typed_data';
 import 'dart:crypto';
+import 'dart:collection';
 
 part 'src/dart_import/util/characters.dart';
 part 'src/dart_import/util/tools.dart';
