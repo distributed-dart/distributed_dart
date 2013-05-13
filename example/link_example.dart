590c3f464a405dc709dfe9c62be4a50a1601bba9
Jacob Bang <julemand101@gmail.com>
2013-05-13 07:06:52 +0200

    link_example updated to use our implementation

--- example/link_example.dart
+++ example/link_example.dart
@@ -1,25 +1,13 @@
 import "dart:io";
 import "dart:async";
+import "package:distributed_dart/distributed_dart.dart" as dist;
 
 void main() {
   Path destination = new Path("example/link_example.link.dart");
   Path source = new Path("example/link_example.dart");
   
-  List<String> arguments = ["/C", 
-                            "mklink", 
-                            "/H", 
-                            destination.toNativePath(), 
-                            source.toNativePath()];
-  
-  Future<ProcessResult> result = Process.run("cmd", arguments);
-  
-  result.then((ProcessResult result) {
-    print("stdout: ${result.stdout}");
-    print("stderr: ${result.stderr}");
-    
-    if (result.stderr.isEmpty) {
-      print("Try delete link.");
-      new File.fromPath(destination).delete().then((_)=>print("Link deleted"));
-    }
+  dist.DartCodeDb.createLink(source, destination).then((_) {
+    print("Try delete link.");
+    new File.fromPath(destination).delete().then((_)=>print("Link deleted"));
   });
 }
