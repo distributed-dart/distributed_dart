78fe0cd8be145b0f408d7d6151cfee1aed2b4e6c
Jacob Bang <julemand101@gmail.com>
2013-05-14 10:04:15 +0200

    Added documentation for DownloadRequest.

--- lib/src/dart_import/download_request.dart
+++ lib/src/dart_import/download_request.dart
@@ -1,13 +1,40 @@
 part of distributed_dart;
 
+/**
+ * Is only used to easier to collect a list of files there are needed to be 
+ * downloaded from another computer. The file to be downloaded is represented 
+ * by the [hash] value and when the file is downloaded it should be saved as 
+ * [hashFilePath]. At last there should be created a link between [hashFilePath] 
+ * and [filePath].
+ */
 class RequestPackage {
+  /// Hash value to request.
   final String hash;
+  
+  /// Path the downloaded file should be saved.
   final Path hashFilePath;
+  
+  /// Path to link there should link to [hashFilePath].
   final Path filePath;
   
+  /// Create a [RequestPackage] object.
   const RequestPackage(this.hash, this.hashFilePath, this.filePath);
   
+  /// Create link a link saved as [filePath] and links to [hashFilePath].
   Future createLink() {
-    DartCodeDb.createLink(this.hashFilePath, this.filePath);
+    return DartCodeDb.createLink(this.hashFilePath, this.filePath);
+  }
+  
+  /// Save [data] into the file [hashFilePath].
+  Future saveFile(List<int> data) {
+    return new File.fromPath(hashFilePath).writeAsBytes(data);
+  }
+  
+  /**
+   * Calls both [saveFile] and [createLink] and returns a future there 
+   * completes when both functions is finish. 
+   */
+  Future saveFileAndCreateLink(List<int> data) {
+    saveFile(data).then((_) => createLink());
   }
 }
