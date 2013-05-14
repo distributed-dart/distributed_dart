63e7d782f2bcae7a69d7c8c512dc8b5c4808695c
Jacob Bang <julemand101@gmail.com>
2013-05-14 20:20:49 +0200

    I think we can read this without comments

--- lib/src/dart_import/exceptions.dart
+++ lib/src/dart_import/exceptions.dart
@@ -1,57 +1,25 @@
 part of distributed_dart;
 
 class InvalidWorkDirException implements Exception {
-  /**
-   * A message describing the error.
-   */
   final String message;
-
-  /**
-   * Creates a new FileChangedException with an optional error [message].
-   */
   const InvalidWorkDirException([this.message = ""]);
-
   String toString() => "InvalidWorkDirException: $message";
 }
 
 class WorkDirInUseException implements Exception {
-  /**
-   * A message describing the error.
-   */
   final String message;
-
-  /**
-   * Creates a new FileChangedException with an optional error [message].
-   */
   const WorkDirInUseException([this.message = ""]);
-
   String toString() => "InvalidWorkDirException: $message";
 }
 
 class FileChangedException implements Exception {
-  /**
-   * A message describing the error.
-   */
   final String message;
-
-  /**
-   * Creates a new FileChangedException with an optional error [message].
-   */
   const FileChangedException([this.message = ""]);
-
   String toString() => "FileChangedException: $message";
 }
 
 class ScannerException implements Exception {
-  /**
-   * A message describing the error.
-   */
   final String message;
-
-  /**
-   * Creates a new ScannerException with an optional error [message].
-   */
   const ScannerException([this.message = ""]);
-
   String toString() => "ScannerException: $message";
 }
