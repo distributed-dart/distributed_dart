893992ce733b112e519657c371ebf94d16f9a57a
Brian Astrup Mikkelsen <brian@mikkelsen.com>
2013-06-23 22:40:44 +0200

    simple example

--- example/master.dart
+++ example/master.dart
@@ -6,20 +6,8 @@ main(){
   logging = true;
   registerNode(new NodeAddress("127.0.0.1", 1234), true, "/tmp/master");
   var slave = new NodeAddress("127.0.0.1", 2345);
-  var sp = spawnUriRemote("./hellodistributedworld.dart",slave);
-
-  var anotherport = new ReceivePort();
-  anotherport.receive((msg,reply) => print("on another port: $msg"));
+  var remote = spawnUriRemote("./hellodistributedworld.dart",slave);
+  //var local = spawnUri("./hellodistributedworld.dart",slave);
   port.receive((msg,reply) => print(msg));
-
-  var data = { 
-    'message' : 'hello distributed dart', 
-    'reply' : anotherport.toSendPort(),
-    'alist' : [ 1 , 23, 4, 'heste' ]
-  };
-
-
-  sp.send("hello my slave", port.toSendPort());
-  sp.send(data, port.toSendPort());
-
+  remote.send("hello remote world",port.toSendPort());
 }
