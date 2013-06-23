cefac4024ac5738ade50c84d33a4b6e4a334ade2
Jacob Bang <julemand101@gmail.com>
2013-06-23 15:46:46 +0200

    _fileRequestHandler code added

--- lib/src/network/handler/filerequesthandler.dart
+++ lib/src/network/handler/filerequesthandler.dart
@@ -3,5 +3,18 @@ part of distributed_dart;
 const String _NETWORK_FILE_REQUEST_HANDLER = "file_request"; 
 
 _fileRequestHandler (dynamic request, NodeAddress senderAddress) {
-  
+  if (request is List) {
+    Map<String,List<int>> requestedFiles = new Map();
+    
+    Future.wait(request.map((String hash) {
+      return _DartCodeDb.getSourceFromHash(hash).then((List<int> fileContent) {
+        requestedFiles[hash] = fileContent;
+      });
+    })).then((_) {
+      new Network(senderAddress).send(_NETWORK_FILE_HANDLER, requestedFiles);
+    });
+  } else {
+    String s = "Not supported input object for _fileRequestHandler.";
+    throw new UnsupportedOperationError(s);
+  }
 }
